98c040b067c4521a6307c4683f85f4040fc5e7b3
Chicodm <133906561+Chicodm@users.noreply.github.com>
2024-05-02 17:11:29 -0300

    Add files via upload

---
+++ java06/aula06/src/TelaHome.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class TelaHome {
+    private JTextField textField0;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton cadastrarButton;
+    private JButton mostrarTodosButton;
+    private JButton localizarButton;
+    private JButton editarButton;
+    private JButton apagarButton;
+    private JButton fakeButton;
+    private JTextArea textArea1;
+}
